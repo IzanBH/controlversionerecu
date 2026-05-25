ab56272e1ad8d2f32b4e7b4a4c16e0ebe822592e
John Doe <johndoe@example.com>
2026-05-25 15:04:57 +0200

    first commit

---
+++ bin/controlversionerecu.dart
@@ -0,0 +1,5 @@
+import 'package:controlversionerecu/controlversionerecu.dart' as controlversionerecu;
+
+void main(List<String> arguments) {
+  print('Hello world: ${controlversionerecu.calculate()}!');
+}
---
+++ test/controlversionerecu_test.dart
@@ -0,0 +1,8 @@
+import 'package:controlversionerecu/controlversionerecu.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
